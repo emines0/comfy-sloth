8ad11dcb4a8829ac6676d56e4d16b79c439db7bc
emines0 <emin.sefa1@gmail.com>
2023-02-27 12:56:20 +0100

    initial files for the project

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import About from './AboutPage'
+import Home from './HomePage'
+import Error from './ErrorPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import AuthWrapper from './AuthWrapper'
+import PrivateRoute from './PrivateRoute'
+
+export {
+  About,
+  Home,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  Checkout,
+  AuthWrapper,
+  PrivateRoute,
+}
