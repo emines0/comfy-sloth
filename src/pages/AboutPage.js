c8083a1ea28cf5eb4825920ca090ed9806dcc4c3
emines0 <emin.sefa1@gmail.com>
2023-02-28 11:40:51 +0100

    implementation of about/checkout page and pageHero(breadcrumbs) component

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,26 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+            labore, veniam repellendus nemo dolorem quod, voluptas facilis
+            voluptatum eum similique rem quae voluptates totam deserunt odit
+            consequuntur delectus corporis ipsum?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
